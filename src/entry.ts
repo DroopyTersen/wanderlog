70ca93c50b2958c924221ff808052d4e2f4e6467
Petersen <apetersen@skylinetechnologies.com>
2020-05-19 00:18:41 -0500

    fixed casing typo in import

--- src/entry.ts
+++ src/entry.ts
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./react/App";
 import "./styles/milligram.custom.scss";
-import "./styles/app.scss";
+import "./styles/App.scss";
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
