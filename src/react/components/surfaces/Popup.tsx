716247e19a41ad9ce914e2988c9efb213431882a
Andrew Petersen <andrew.petersen15@gmail.com>
2020-08-19 07:23:57 -0600

    styled menu close button

--- src/react/components/surfaces/Popup.tsx
+++ src/react/components/surfaces/Popup.tsx
@@ -6,7 +6,7 @@ export default function Popup({ isOpen, close, title, children, className = "" }
   return (
     <div className={["popup", className, isOpen ? "open" : "closed"].filter(Boolean).join(" ")}>
       <Button className="popup-close" onClick={close}>
-        close
+        x
       </Button>
       {title && <h2 className="popup-title">{title}</h2>}
       <div className="popup-content">{children}</div>
