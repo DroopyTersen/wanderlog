b7ed738c5da5843b21a565370a0b428f1050c315
Andrew Petersen <andrew.petersen15@gmail.com>
2020-12-16 19:31:23 -0600

    animated header shadow

--- src/global/components/Header/Header.tsx
+++ src/global/components/Header/Header.tsx
@@ -2,9 +2,27 @@ import React, { useRef } from "react";
 import "./Header.scss";
 import { Link, useRoutes, useLocation, useNavigate } from "react-router-dom";
 import { MdArrowBack } from "react-icons/md";
+import { motion, useMotionTemplate, useTransform, useViewportScroll } from "framer-motion";
 
+const variants = {
+  shadowed: {
+    boxShadow: "0 8px 20px -11px rgba(0,0,0,.25)",
+  },
+  unshadowed: {
+    boxShadow: "none",
+  },
+};
+function useHeaderShadow() {
+  let { scrollY } = useViewportScroll();
+  let boxShadowOpacity = useTransform(scrollY, [0, 50], [0, 0.3]);
+  let boxShadow = useMotionTemplate`0 8px 20px -11px rgba(0,0,0,${boxShadowOpacity})`;
+  return {
+    boxShadow,
+  };
+}
 export function Header({ title = "Wanderlog" }) {
   let { pathname } = useLocation();
+  let style = useHeaderShadow();
   let backLink = "";
   if (pathname !== "/") {
     backLink = "/" + pathname.substr(0, pathname.lastIndexOf("/"));
@@ -12,7 +30,7 @@ export function Header({ title = "Wanderlog" }) {
   // console.log("Back link ", backLink, pathname);
 
   return (
-    <div className="header">
+    <motion.div className="header" style={style}>
       {backLink ? (
         <Link to={backLink} className="back">
           <MdArrowBack size={22} />
@@ -22,6 +40,6 @@ export function Header({ title = "Wanderlog" }) {
       )}
       <h1 className="header-title">{title}</h1>
       <div></div>
-    </div>
+    </motion.div>
   );
 }
